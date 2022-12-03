890b5c276dab0bb26edfda1e5959e89923b03491
alirafli <mrafli1212@gmail.com>
2022-12-03 10:44:34 +0700

    add: header

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import { StatusBar } from "expo-status-bar";
-import {Text, View } from "react-native";
+import React, { useState } from "react";
+import { View, FlatList } from "react-native";
+import Header from "./components/header";
+import TodoItem from "./components/todoItem";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: "Buy food in supermarket", key: "1" },
+    { text: "read novel in night", key: "2" },
+    { text: "cook some food for dinner", key: "3" },
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prev) => {
+      return prev.filter(todo => todo.key != key)
+    })
+  }
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="bg-red-300">testing</Text>
-      <Text>testing</Text>
-      <StatusBar style="auto" />
+    <View className="">
+      <Header />
+      <View className="mt-10 mx-4">
+        <View>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <TodoItem item={item} pressHandler={pressHandler} />}
+          />
+        </View>
+      </View>
     </View>
   );
 }
-
